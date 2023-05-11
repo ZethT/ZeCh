803f6c1efa5813488663a78303c5f5963a48f168
ZethT <zethtang@gmail.com>
2023-05-11 16:59:26 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import Header from "./Components/Header";
+
+function App(){
+   return <div><Header/></div>;
+}
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,6 @@
+//PascalCasing
+function Header(){
+
+    return <h1>Hellooooooo</h1>;
+}
+export default Header;
